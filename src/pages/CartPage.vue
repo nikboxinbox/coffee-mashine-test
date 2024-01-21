4d6cec5a9d89b80fe1499083485a9bce6d96222b
nik_nik <nikbox88@gmail.com>
2024-01-21 13:51:50 +0300

    edit styles CartPage

--- src/pages/CartPage.vue
+++ src/pages/CartPage.vue
@@ -1,54 +1,68 @@
 <template lang="pug">
-
-h1 Корзина
-div(v-for="item in cart.listModels" :key="item.name")
-    span {{ item.title }} 
-    span количество: {{ item.count }}
+.cart  
+  h1  Корзина
+  h2(v-if="!cart.listModels.length") нет выбранных позиций
+  .cart__item(
+    v-else 
+    v-for="item in cart.listModels" 
+    :key="item.name")
+    span {{ item.title }}
+    span {{ item.count }} шт
     button(@click="cart.addToCart(item.name)") Добавить
     button(@click="cart.removeFromCart(item.name)") Удалить
+
 router-link.router-link(to="/") Назад
-
-
 </template>
+
 <script setup>
-import { computed, ref } from 'vue'
-import { storeToRefs } from 'pinia'
 import { useMainStore } from '../stores/mainStore'
 
-const { cart } = storeToRefs(useMainStore())
-
-// const list = computed(() => cart.value.getListMashines())
+const { cart } = useMainStore()
 </script>
 
 <style scoped>
+
 div {
-  margin-bottom: 10px;
   padding: 10px;
   border: 1px solid #ddd;
   border-radius: 5px;
-  font-size: 16px;
-  width: fit-content;
+  font-size: 18px;
+}
+
+.cart__item {
+  display: grid;
+  grid-template-columns: 150px 1fr 100px 100px;
+  grid-column-gap: 5px;
+  margin-bottom: 5px;
 }
 
 span {
   margin-right: 10px;
+  width: 100px;
 }
 
 button {
-  /* margin-top: 5px; */
   padding: 5px;
   background-color: #4caf50;
-  color: white;
+  color: rgba(255, 254, 254, 0.786);
   border: none;
   border-radius: 3px;
   cursor: pointer;
-  margin-right: 10px;
 }
 
 button:hover {
   background-color: #45a049;
 }
+
+button:active {
+  background-color: rgb(8, 106, 63);
+}
 .router-link {
   border-radius: 5px;
+  display: block;
+  margin-top: 10px;
+  padding: 10px 0;
+  text-align: center;
+  font-size: 20px;
 }
 </style>
