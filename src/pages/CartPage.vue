2856b078760f66f7ff9964cada871954b759b779
nik_nik <nikbox88@gmail.com>
2024-01-21 04:25:13 +0300

    + cardPage

---
+++ src/pages/CartPage.vue
@@ -0,0 +1,54 @@
+<template lang="pug">
+
+h1 Корзина
+div(v-for="item in cart.listModels" :key="item.name")
+    span {{ item.title }} 
+    span количество: {{ item.count }}
+    button(@click="cart.addToCart(item.name)") Добавить
+    button(@click="cart.removeFromCart(item.name)") Удалить
+router-link.router-link(to="/") Назад
+
+
+</template>
+<script setup>
+import { computed, ref } from 'vue'
+import { storeToRefs } from 'pinia'
+import { useMainStore } from '../stores/mainStore'
+
+const { cart } = storeToRefs(useMainStore())
+
+// const list = computed(() => cart.value.getListMashines())
+</script>
+
+<style scoped>
+div {
+  margin-bottom: 10px;
+  padding: 10px;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  font-size: 16px;
+  width: fit-content;
+}
+
+span {
+  margin-right: 10px;
+}
+
+button {
+  /* margin-top: 5px; */
+  padding: 5px;
+  background-color: #4caf50;
+  color: white;
+  border: none;
+  border-radius: 3px;
+  cursor: pointer;
+  margin-right: 10px;
+}
+
+button:hover {
+  background-color: #45a049;
+}
+.router-link {
+  border-radius: 5px;
+}
+</style>
